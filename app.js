2c5f6aa89c08c45098926b90713c7d1f50b5d86a
Qasim Ali <54708575+qasimlucky@users.noreply.github.com>
2023-05-26 20:54:59 +0500

    first commit

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
   next();
 });
 //app.use(cors());
-app.use(cors({ credentials: true, origin: '*' }));
+//app.use(cors({ credentials: true, origin: '*' }));
 //app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 //app.use(cors({ credentials: true, origin: 'https://salom-vercel-deploy-9g6g.vercel.app/' }));
 
